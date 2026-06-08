ec3a8cafc34d33bccee0850d2425b14ab958f838
Javier <jluqueserrano00@gmail.com>
2026-06-09 01:54:51 +0200

    Aumentar recursos de Picasso

--- scripts/script.sh
+++ scripts/script.sh
@@ -14,8 +14,8 @@
 #SBATCH --job-name=rnaseq_nf
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=2
-#SBATCH --mem=8G
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=32G
 #SBATCH --time=48:00:00
 #SBATCH --output=logs/nf_%j.out
 #SBATCH --error=logs/nf_%j.err
